adb07e2527a8ea5cba9aec0f95a09bb335a294e1
Cloud Chen <pk14116@gmail.com>
2020-10-03 00:43:23 +0800

    feat(): implement done

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import yaml
 from datetime import datetime
 import csv
 import subprocess
+import os
 from threading import Thread
 from queue import Queue
-from page1 import Page1
-from default_page import DefaultPage
+from page import Page
 from page_factory import PageFactory
 
 NUM_THREADS = 10
@@ -28,6 +28,8 @@ def load_config():
 
 
 def get_server_list_from_config():
+    print("{csv_file_name} not found... "
+          "ip list source : config.yaml\n".format(csv_file_name=config['CSV_FILE_NAME']))
     server_list = config['SERVER_LIST']
     if server_list == None:
         print("ERROR! IP_LIST in config.yaml is empty... please check")
@@ -37,6 +39,8 @@ def get_server_list_from_config():
 
 
 def get_server_list_from_csv():
+    print("{csv_file_name} found... "
+          "ip list source : {csv_file_name}\n".format(csv_file_name=config['CSV_FILE_NAME']))
     try:
         with open(config['CSV_FILE_NAME'], 'r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
@@ -45,12 +49,9 @@ def get_server_list_from_csv():
                 server_list.append(
                     {'IP': row[0].strip(),
                      'TYPE': row[1].strip()})
-            print("{csv_file_name} found. "
-                  "read ip list from {csv_file_name}.\n".format(csv_file_name=config['CSV_FILE_NAME']))
     except FileNotFoundError:
-        print("{csv_file_name} not found... "
-              "read ip list from config.yaml instead.\n".format(csv_file_name=config['CSV_FILE_NAME']))
-    if len(server_list.get('SERVER_LIST')) == 0:
+        print("BUG HERE! should not got here.")
+    if len(server_list) == 0:
         print("ERROR! {csv_file_name}'s content is empty... please check".format(
             csv_file_name=config['CSV_FILE_NAME']))
         input("press Enter key to close this window...")
@@ -85,16 +86,6 @@ def get_reachable_servers(server_list: dict):
     print("================= End of ping =================")
 
 
-def generate_report(report: dict, server_cnt: int, html_id: dict):
-    report[server_cnt] = {}
-    for key in html_id.keys():
-        try:
-            report[server_cnt][key] = soup.find(id=html_id.get(key)).text
-        except AttributeError:
-            report[server_cnt][key] = None  # 找不到該元素時於報告中填上 null
-    return report
-
-
 requests.packages.urllib3.disable_warnings()
 config = load_config()
 report = {}
@@ -103,51 +94,26 @@ timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 is_ip_from_csv = True if os.path.isfile(
     config['CSV_FILE_NAME']) is True else False
 if(is_ip_from_csv):
-    print("{csv_file_name} found... "
-          "ip list source : {csv_file_name}\n".format(csv_file_name=config['CSV_FILE_NAME']))
     server_list = get_server_list_from_csv()
 else:
-    print("{csv_file_name} not found... "
-          "ip list source : config.yaml\n".format(csv_file_name=config['CSV_FILE_NAME']))
     server_list = get_server_list_from_config()
 reachable_servers = []
 # 利用多線程同時 ping 多個 server，並將有 ping 到的加進 reachable_servers
 get_reachable_servers(server_list)
-page_refactory = PageFactory()
-
+page_refactory = PageFactory(config)
+pages = []
 for server in reachable_servers:
     ip = server.get('IP')
     page_type = server.get('TYPE')
-    url = config['URL_PATTERN'].format(ip=ip)
     server_cnt += 1
-    print("start proccessing server {server_cnt}:".format(
-        server_cnt=server_cnt))
-    try:
-        resp = requests.get(url, verify=False, timeout=config['TIMEOUT'])
-    except requests.exceptions.ConnectTimeout:
-        print("ConnectTimeout! can't not get html content from {ip} after {timeout} seconds.\n"
-              "(maybe try to increase more time to wait)\n".format(
-                  ip=ip, timeout=config['TIMEOUT']))
-        continue
-    except requests.exceptions.ConnectionError:
-        print("ConnectionError! can't not get html content from {ip}.\n"
-              "(this ip may not a HP printer server)\n".format(
-                  ip=ip, timeout=config['TIMEOUT']))
-        continue
-
-    print(".")
-    resp.encoding = 'utf-8'
-    soup = BeautifulSoup(resp.text, 'lxml')
-    print("..")
-    report = generate_report(
-        report=report, server_cnt=server_cnt, html_id=config.get('PAGE').get(page_type))
-    print("...")
-
-
+    pages.append((server_cnt, page_refactory.get_page_instance(page_type, ip)))
+result = {}
+for page in pages:
+    result[page[0]] = page[1].get_crawler_result()
 file_name = '{timestamp}_report.txt'.format(
     timestamp=timestamp)
 with open(file_name, 'w', encoding='utf-8') as stream:
-    stream.write(yaml.dump(report, allow_unicode=True))
+    stream.write(yaml.dump(result, allow_unicode=True))
 
 input("\n===============================================================================\n"
       "Report was been generated, please check file =====> {timestamp}_report.yaml"
--- page_factory.py
+++ page_factory.py
@@ -1,11 +1,11 @@
-import Page1
-import DefaultPage
+from page import Page 
 
 
 class PageFactory():
 
-    def get_page_instance(self, page_type):
+    def __init__(self, config):
+        self.config = config
+
+    def get_page_instance(self, page_type, ip):
         if (page_type == 'TYPE_1'):  # 頁面命名的方式可能要再想，類似 English 或是 type1
-            return Page1()
-        else:
-            return DefaultPage()
+            return Page(self.config, page_type, ip)
