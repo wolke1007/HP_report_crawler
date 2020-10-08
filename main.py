c5f62b01251b54e92e02d395877f0cbdfc497075
cloudchen_mitake_mac <cloud.chen@mitake.com.tw>
2020-10-08 18:27:50 +0800

    refactor
    1. add USING_CSV variable
    2. add if __name__ == '__main__'
    3. add doc string
    4. delete PageFactory because no longer use this pattern at all
    5. add error handling if can not get type of page in config when crawling

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ import os
 from threading import Thread
 from queue import Queue
 from page import Page
-from page_factory import PageFactory
 import time
 
 NUM_THREADS = 5
 
 
 def load_config():
+    """
+    讀取 config.yaml
+    """
     try:
         with open('config.yaml', 'r', encoding='utf-8') as stream:
             config = yaml.safe_load(stream)
@@ -28,7 +30,9 @@ def load_config():
 
 
 def get_server_list_from_config():
-    # 從 config.yaml 檔中讀取 SERVER_LIST 的值
+    """
+    從 config.yaml 檔中讀取 SERVER_LIST 的值
+    """
     print("{csv_file_name} not found... "
           "ip list source : config.yaml\n".format(csv_file_name=config['CSV_FILE_NAME']))
     server_list = config['SERVER_LIST']
@@ -40,7 +44,13 @@ def get_server_list_from_config():
 
 
 def get_server_list_from_csv():
-    # 從 config.yaml 中指定的 csv 檔中取資料
+    """
+    從 config.yaml 中指定的 csv 檔中取資料
+    資料格式為:
+        192.168.2.126 TYPE_1
+        192.168.2.12 TYPE_2
+    這樣的形式紀錄
+    """
     print("{csv_file_name} found. "
           "ip list source : {csv_file_name}\n".format(csv_file_name=config['CSV_FILE_NAME']))
     try:
@@ -49,8 +59,8 @@ def get_server_list_from_csv():
             server_list = []
             for row in reader:
                 server_list.append(
-                    {'IP': row[0].strip(),
-                     'TYPE': row[1].strip()})
+                    {'IP': row[0].strip(),  # 資料的第 0 個資料是 IP
+                     'TYPE': row[1].strip()})  # 資料的第 1 個資料是 TYPE
     except FileNotFoundError:
         print("BUG HERE! should not got here.")
     if len(server_list) == 0:
@@ -62,7 +72,9 @@ def get_server_list_from_csv():
 
 
 def pingme(thread, queue: Queue):
-    # ping server 主要邏輯
+    """
+    ping server 主要邏輯
+    """
     while True:
         server = queue.get()
         ip = server.get('IP')
@@ -77,7 +89,9 @@ def pingme(thread, queue: Queue):
 
 
 def get_reachable_servers(server_list: list):
-    # ping server 多執行緒部分的邏輯
+    """
+    ping server 多執行緒部分的邏輯
+    """
     print("=========== Start ping all servers ===========")
     queue = Queue()
     for thread in range(NUM_THREADS):
@@ -91,6 +105,9 @@ def get_reachable_servers(server_list: list):
 
 
 def crwaling(thread, queue: Queue):
+    """
+    爬蟲主要邏輯在 page.get_all_element_from_html() 中實現
+    """
     server = queue.get()
     if (server):
         page = server['page']
@@ -102,7 +119,10 @@ def crwaling(thread, queue: Queue):
         queue.task_done()
 
 
-def get_pages_content(reachable_servers, page_factory, pages):
+def get_pages_content(reachable_servers: list, config, pages: list):
+    """
+    爬蟲多執行緒部分的邏輯
+    """
     print("=========== Start crawling all pages ===========")
     queue = Queue()
     threads = []
@@ -112,42 +132,41 @@ def get_pages_content(reachable_servers, page_factory, pages):
         new_thread.start()
         threads.append(new_thread)
     for server in reachable_servers:
-        page = page_factory.get_page_instance(server.get('TYPE'),
-                                              server.get('IP'))
+        page = Page(config=config, page_type=server.get('TYPE'), 
+                    ip=server.get('IP'))
         queue.put({'server': server,
-                   'page_factory': page_factory,
                    'page': page,
                    'pages': pages})
     queue.join()
     print("================= End of crawling =================")
 
 
-requests.packages.urllib3.disable_warnings()
-config = load_config()
-report = {}
-server_cnt = 0
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-is_ip_from_csv = True if os.path.isfile(
-    config['CSV_FILE_NAME']) is True else False
-if(is_ip_from_csv):
-    server_list = get_server_list_from_csv()
-else:
-    server_list = get_server_list_from_config()
-reachable_servers = []
-# 利用多線程同時 ping 多個 server，並將有 ping 到的加進 reachable_servers
-get_reachable_servers(server_list)
-page_factory = PageFactory(config)
-pages = []
-get_pages_content(reachable_servers, page_factory, pages)
-result = {}
-for num, page in enumerate(pages, start=1):
-    result[num] = page.get_crawler_result()
-file_name = '{timestamp}_report.txt'.format(
-    timestamp=timestamp)
-with open(file_name, 'w', encoding='utf-8') as stream:
-    stream.write(yaml.dump(result, allow_unicode=True))
+if __name__ == '__main__':
+    requests.packages.urllib3.disable_warnings()
+    config = load_config()
+    report = {}
+    server_cnt = 0
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    is_ip_from_csv = True if os.path.isfile(
+        config['CSV_FILE_NAME']) is True else False
+    if(is_ip_from_csv and config.get('USING_CSV')):
+        server_list = get_server_list_from_csv()
+    else:
+        server_list = get_server_list_from_config()
+    reachable_servers = []
+    # 利用多線程同時 ping 多個 server，並將有 ping 到的加進 reachable_servers
+    get_reachable_servers(server_list)
+    pages = []
+    get_pages_content(reachable_servers, config, pages)
+    result = {}
+    for num, page in enumerate(pages, start=1):
+        result[num] = page.get_crawler_result()
+    file_name = '{timestamp}_report.txt'.format(
+        timestamp=timestamp)
+    with open(file_name, 'w', encoding='utf-8') as stream:
+        stream.write(yaml.dump(result, allow_unicode=True))
 
-input("\n===============================================================================\n"
-      "Report was been generated, please check file =====> {timestamp}_report.yaml"
-      "\n===============================================================================\n"
-      "press Enter key to close this window...".format(timestamp=timestamp))
+    input("\n===============================================================================\n"
+        "Report was been generated, please check file =====> {timestamp}_report.yaml"
+        "\n===============================================================================\n"
+        "press Enter key to close this window...".format(timestamp=timestamp))
--- page.py
+++ page.py
@@ -13,6 +13,10 @@ class Page():
         self.crawler_result = {}
 
     def get_all_element_from_html(self):
+        """
+        這邊為爬蟲主要邏輯實現，當中使用 soup.find(id) 的方式爬
+        如果有遇到頁面的 id 為重複個且內容不為預期的時候會有問題需要另外處理之
+        """
         url = self.config['URL_PATTERN'].format(ip=self.ip)
         try:
             resp = requests.get(url, verify=False, timeout=self.config['TIMEOUT'])
@@ -36,6 +40,26 @@ class Page():
             print("ConnectionError! can't not get html content from {ip}.\n"
                 "(this ip may not a HP printer server)\n".format(
                     ip=self.ip, timeout=self.config['TIMEOUT']))
-        
+        except KeyError:
+            print('KeyError! please check page type:"{key}" is exist in config.yaml'.format(key=key))
+            input("press Enter key to close this window...")
+            exit()
+
     def get_crawler_result(self):
+        """
+        回傳此頁面爬蟲後的結果
+        儲存格式為 dict，之後轉 yaml 後就會以這樣的形式呈現：
+        {
+            1:
+                key1: value1
+                key2: value2
+            2:
+                key1: value1
+                key2: value2
+        }
+        """
         return self.crawler_result
+
+
+if __name__ == '__main__':
+    exit()
